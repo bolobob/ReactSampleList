77ba7c59428cc317f623dad241cd136740e5abde
bolobob <aokishinichi@gmail.com>
2023-03-18 11:45:46 +0900

    不要なコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Page from "./features/PropsHeading";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Page />

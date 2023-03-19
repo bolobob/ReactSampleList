44e054da2d8ecf1974f46b96a3f1a52527a57b5b
bolobob <aokishinichi@gmail.com>
2023-03-19 16:36:15 +0900

    明示的にlevelを指定しなくもいいように

--- src/features/context/components/Section.tsx
+++ src/features/context/components/Section.tsx
@@ -1,17 +1,14 @@
-import { ReactNode } from "react";
+import { ReactNode, useContext } from "react";
 import clsx from "clsx";
 import { LevelContext } from "../contexts/LevelContext";
 
-export default function Section({
-  level,
-  children,
-}: {
-  level: number;
-  children: ReactNode;
-}) {
+export default function Section({ children }: { children: ReactNode }) {
+  const level = useContext(LevelContext);
   return (
     <section className={clsx("section")}>
-      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
     </section>
   );
 }

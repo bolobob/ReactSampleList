7eb371f0e76ddbcff102a4b1b7dc10061824f705
bolobob <aokishinichi@gmail.com>
2023-03-21 02:17:38 +0900

    js, css へのパスがこれで直る？

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "react-sample-list" // レポジトリ名を設定
+    : "./",
   plugins: [react()],
 });

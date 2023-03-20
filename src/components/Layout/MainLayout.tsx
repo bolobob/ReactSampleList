fdbfebc28b29756d4a164337a7a0073941b0ee89
bolobob <aokishinichi@gmail.com>
2023-03-21 02:01:42 +0900

    サイドバーの中身を仮置

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -12,7 +12,14 @@ export const MainLayout = ({ children }: { children: ReactNode }) => {
         "h-screen",
       ])}
     >
-      <div className={clsx(["basis-80", "grow"])}>saidebar</div>
+      <div className={clsx(["basis-80", "grow"])}>
+        <nav>
+          <ul className={clsx(["list-none", "list-inside"])}>
+            <li>コンテキスト</li>
+            <li>コンテキスト（関係ないコンポーネントが混ざってる）</li>
+          </ul>
+        </nav>
+      </div>
       <div
         className={clsx(["flex", "justify-center", "basis-0", "grow-[999]"])}
       >

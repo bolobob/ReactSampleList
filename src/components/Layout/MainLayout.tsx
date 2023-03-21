dc4e7e418ca37f139913de7612d472df975db30d
bolobob <aokishinichi@gmail.com>
2023-03-21 18:52:33 +0900

    refactoring

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,30 +1,23 @@
 import clsx from "clsx";
 import { ReactNode } from "react";
-import { Link, PathMatch, useMatch } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 
-const SidebarItem = ({
-  matchPath,
-  to,
-  text,
-}: {
-  matchPath: ReturnType<typeof useMatch>;
-  to: string;
-  text: string;
-}) => (
-  <>
-    {matchPath?.pathname.match(new RegExp(to)) ? (
-      <>{text}</>
-    ) : (
-      <Link className={clsx(["text-blue-500", "hover:underline"])} to={to}>
-        {text}
-      </Link>
-    )}
-  </>
-);
+const SidebarItem = ({ to, text }: { to: string; text: string }) => {
+  const matchPath = useMatch({ path: location.pathname });
+  return (
+    <>
+      {matchPath?.pathname.match(new RegExp(to)) ? (
+        <>{text}</>
+      ) : (
+        <Link className={clsx(["text-blue-500", "hover:underline"])} to={to}>
+          {text}
+        </Link>
+      )}
+    </>
+  );
+};
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
-  const matchPath = useMatch({ path: location.pathname });
-
   return (
     <div
       className={clsx([
@@ -40,14 +33,12 @@ export const MainLayout = ({ children }: { children: ReactNode }) => {
           <ul className={clsx(["list-disc", "list-inside"])}>
             <li className={clsx(["px-5"])}>
               <SidebarItem
-                matchPath={matchPath}
                 to="/context/props_headings"
                 text="useContextを使ってみた"
               />
             </li>
             <li className={clsx(["px-5"])}>
               <SidebarItem
-                matchPath={matchPath}
                 to="/context/my_profile"
                 text="useContextを使ってみた（関係ないコンポーネントが混ざってる）"
               />

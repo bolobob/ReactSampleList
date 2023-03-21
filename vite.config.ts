f7d0e8cac5d5c5d7ea2d13e511665854db173d76
bolobob <aokishinichi@gmail.com>
2023-03-21 11:49:41 +0900

    デプロイ失敗してる原因を突き止めるため、ここか？

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.BASE_URL,
+  base: process.env.GITHUB_PAGES
+    ? "react-sample-list" // レポジトリ名を設定
+    : "./",
   plugins: [react()],
 });
